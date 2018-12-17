73fb6b419b2f0e391e6e793b0b687607aac352ba
nxycdl <nxycdl@163.com>
2018-12-17 10:21:20 +0800

    first commit

---
+++ src/main/java/com/ycfddl/ZuulServiceApplication.java
@@ -0,0 +1,19 @@
+package com.ycfddl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+	// @EnableZuulProxy 代表网关代理
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
+
